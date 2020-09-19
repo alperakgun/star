af171f8ebf8fb2c9fe421ada93504ba3b931e6be
Alper Akgun <alper.akgun@sayarus.com>
2020-09-19 12:47:35 +0300

    Updates

---
+++ precache-manifest.644bd67145d38cb13cd3caee87851fd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1f129a3e03a754a341dbb368b39537",
+    "url": "/star/index.html"
+  },
+  {
+    "revision": "82b6f958bef8d05a4e02",
+    "url": "/star/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7638c4b924b856b369a8",
+    "url": "/star/static/js/2.6edf5299.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star/static/js/2.6edf5299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b6f958bef8d05a4e02",
+    "url": "/star/static/js/main.2e355cf1.chunk.js"
+  },
+  {
+    "revision": "014d46f7f2b6838e5faf",
+    "url": "/star/static/js/runtime-main.f747a260.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star/static/media/logo.5d5d9eef.svg"
+  }
+]);
